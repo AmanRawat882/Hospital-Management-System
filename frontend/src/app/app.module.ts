9e6b158b51041dc4c6a65757596a5d930272ccc1
Aman <amanrawat882@gmail.com>
2021-06-22 12:59:51 +0530

    Components Created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './home/header/header.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { BookingComponent } from './booking/booking.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BookingComponent
+    BookingComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    AdminPanelComponent
   ],
   imports: [
     BrowserModule,
