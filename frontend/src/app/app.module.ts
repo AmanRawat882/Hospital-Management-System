1b9f40e6ab914259d23c1643f864ef23fe4f3582
Aman <amanrawat882@gmail.com>
2021-06-22 12:56:29 +0530

    Dummy Header And footer Creater

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
