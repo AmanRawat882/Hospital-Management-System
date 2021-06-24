a0813f283538a5ea48ad291f7a134dd80a779687
Kashaf Siddiqui <siddiquikashaf786@gmail.com>
2021-06-24 13:06:35 +0530

    Added Form service to send data to backend

--- frontend/src/app/booking/booking.component.ts
+++ frontend/src/app/booking/booking.component.ts
@@ -1,5 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Doctor } from '../doctor';
+import { DoctorService } from '../doctor.service';
+import { Patient } from '../patient';
+import { PatientService } from '../patient.service';
 
 
 @Component({
@@ -9,22 +14,36 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 
 export class BookingComponent{
+  doctors!:Doctor[];
+  patientId!: number;
+  constructor(private fb:FormBuilder,private doctorService:DoctorService,private patientService:PatientService){}
   profileForm = new FormGroup({
-    fullName: new FormControl(''),
+    name: new FormControl(''),
     complaint: new FormControl(''),
     appointmentDate:new FormControl(''),
-    bill: new FormControl(''),
     doctor: new FormControl({
-    nameofdoc: new FormControl(''),
-    speciality: new FormControl(''),
-    fees:new FormControl(''),
-    slots:new FormControl('')
+    id:new FormControl(null),
+    name: new FormControl('')
   }),
-  admitted:new FormControl(''),
+  isAdmitted:new FormControl(''),
   });
   onSubmit() {
+    this.profileForm.value.id='';
+    this.profileForm.value.bill='';
     console.warn(this.profileForm.value);
-    alert("Your Form has been submitted successfully");
-    this.profileForm.reset();
+    this.addAppointment(this.profileForm.value);
+  }
+  
+  public getDoctors():void{
+    this.doctorService.getDoctors().subscribe(
+      (respose: Doctor[])=>{this.doctors=respose;}
+    );
+  }
+  public addAppointment(formValue:Patient):void{
+    this.patientService.addAppointment(formValue).subscribe((response:number)=>{this.patientId=response;},(error:HttpErrorResponse)=>{alert(error.message);});
+  }
+
+  ngOnInit(): void {
+    this.getDoctors();
   }
 }
